b9ed0b846f2b13503c085ffc33cdbee581eca9ea
MrPenguinNootNoot <ben@trbnet.com>
2026-03-08 16:26:42 +1100

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC3d_olB4pzwfP9qsZATHSzeRYeu1WwpVI",
+  authDomain: "noot-1b1c7.firebaseapp.com",
+  projectId: "noot-1b1c7",
+  storageBucket: "noot-1b1c7.firebasestorage.app",
+  messagingSenderId: "199803972526",
+  appId: "1:199803972526:web:1ffebd5f5bae8cfbc5f63f",
+  measurementId: "G-WRWMZYZ7Y0"
 };
 
 export { firebaseConfig };
